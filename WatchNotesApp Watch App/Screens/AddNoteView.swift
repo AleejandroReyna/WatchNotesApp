ef0af104d3c262b5341fedd2de0dc10387f690bf
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-29 23:53:07 -0600

    views: Added all content in a VStack

--- WatchNotesApp Watch App/Screens/AddNoteView.swift
+++ WatchNotesApp Watch App/Screens/AddNoteView.swift
@@ -11,14 +11,16 @@ struct AddNoteView: View {
     @State private var notes : [Note] = []
     @State private var text : String = ""
     var body: some View {
-        TextField("Title", text: $text)
-        Button("Add note") {
-            guard text.isEmpty == false else {
-                return
+        VStack {
+            TextField("Title", text: $text)
+            Button("Add note") {
+                guard text.isEmpty == false else {
+                    return
+                }
+                let note  = Note(title: text)
+                notes.append(note)
+                text = ""
             }
-            let note  = Note(title: text)
-            notes.append(note)
-            text = ""
         }
     }
 }
