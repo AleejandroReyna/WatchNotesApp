dea954a8a140cf856d2ae38eed19af392d7857a8
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-29 23:08:13 -0600

    Initial Commit

---
+++ WatchNotesApp Watch App/WatchNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNotesAppApp.swift
+//  WatchNotesApp Watch App
+//
+//  Created by Alejandro Reyna on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotesApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
